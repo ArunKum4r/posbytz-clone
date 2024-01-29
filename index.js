00b1f0e5c41d6a5ccea0a799f5ce24de6faf0c30
ArunKum4r <arunvjr107@gmail.com>
2024-01-29 19:47:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const customers = [
+  "https://posbytz.com/wp-content/uploads/2023/09/maaza.png",
+  "https://posbytz.com/wp-content/uploads/2023/09/Kebab-time.png",
+  "https://posbytz.com/wp-content/uploads/2023/02/shero-01.png",
+  "https://posbytz.com/wp-content/uploads/2023/07/msfood.png",
+  "https://posbytz.com/wp-content/uploads/2023/09/Al-karakand-e1695319053772.png",
+  "https://posbytz.com/wp-content/uploads/2023/09/Jazz-cafe.jpg",
+  "https://posbytz.com/wp-content/uploads/2023/09/HOT-POT.png",
+  "https://posbytz.com/wp-content/uploads/2023/09/Diamond-Restaurant.png",
+];
+
+const testimonials = [
+  {
+    img: "https://posbytz.com/wp-content/uploads/2023/09/maaza-85x90.png",
+    name: "MazaArabia, KSA",
+    review:
+      "We are providing broast chicken food across KSA with about 25 outlets and growing. With posbytz we have automated the entire operations from purchases, inventory, sales and also enabled our own online ordering system with ease. We will definetly recomended PosBytz to all restaurant ERP Software to F&B brands expanding as franchise business",
+  },
+  {
+    img: "https://posbytz.com/wp-content/uploads/2021/09/posbytz_review_farmarika.webp",
+    name: "Farmarika",
+    review:
+      "This ERP platform is simple and excellent . It is has the omnichannel model to sell the products. I have been using this product for more than 1 year. It is very good to manage the catalogue, items, pricing, taxing , customers, employees etc",
+  },
+  {
+    img: "https://posbytz.com/wp-content/uploads/2021/09/tehran_taste_persia-85x90.png",
+    name: "Tehran - The Taste of Persia",
+    review:
+      "I have been using PosBytz for more than two years, would definetly recommend to all my dellow restaurant owners given the fact it gives complete control over your business. Best would be the branded apps and online ordering platform integrated with POS and inventory.",
+  },
+  {
+    img: "https://posbytz.com/wp-content/uploads/2023/09/Al-karakand-e1695319023203-83x71.png",
+    name: "Alkarkand Abha KSA",
+    review:
+      "We have rceently migrated our software to PosBytz so far have been always improvising the features based on request an d strongly recommend for F&B groups managing complex restaurant operations with multiple brands from purchases, inventory, transfers, Accounting to P&L reports. Integrated QR code ordering system with POS works seamlessly and gives a great customer experience.",
+  },
+  {
+    img: "https://posbytz.com/wp-content/uploads/2023/09/Sams-Pizza-85x90.png",
+    name: "Sam's Pasta Pizza, Qatar",
+    review:
+      "We are an traditional Italian cuisine based brand. Have been using PosBytz for our Restaurant Management operations and so far there has been no issues with respect to the software and as a owner can able see my business online from anywhere. PosBytz is very easy to use for setting up menu , ingredients/recipes , POS , inventory etc..,",
+  },
+];
